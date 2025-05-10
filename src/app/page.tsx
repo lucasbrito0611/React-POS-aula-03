6d9c27f434590b65b258150aaa535fc6773a8a06
lucasbrito0611 <lucasbrito2006@hotmail.com>
2025-05-10 19:56:02 -0300

    Funcionalidade de adicionar tarefa (sem persistência de dados)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,34 @@ import type React from "react";
 import { useState } from "react";
 import dados, { TarefaInterface } from "@/data";
 import Cabecalho from "@/componentes/Cabecalho";
-import Tarefa from "@/componentes/Tarefa";
 import Tarefas from "@/componentes/Tarefas";
+import ModalTarefa from "@/componentes/ModalTarefa";
 
 const Home = () => {
-	const tarefas: TarefaInterface[] = dados;
+	const [tarefas, setTarefas] = useState<TarefaInterface[]>(dados);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const adicionarTarefa = (titulo: string) => {
+		if (titulo.trim() === '') {
+			alert("Título da tarefa não pode ser vazio.");
+			return;
+		}
+		setTarefas([
+			{ id: tarefas.length + 1, title: titulo, completed: false },
+			...tarefas
+		]);
+		setIsModalOpen(false);
+	};
 
 	return (
-		<div className="container mx-auto p-4">
+		<div className="flex flex-col gap-8 container mx-auto p-4">
 			<Cabecalho />
+			<div>
+				<button className="bg-gray-800 text-white text-2xl font-medium py-4 px-6 rounded-[5px] cursor-pointer" onClick={() => setIsModalOpen(true)}>
+					Nova tarefa
+				</button>
+			</div>
+			{isModalOpen && <ModalTarefa addTarefa={adicionarTarefa} onClose={() => setIsModalOpen(false)} />}
 			<Tarefas dados={tarefas} />
 		</div>
 	);
