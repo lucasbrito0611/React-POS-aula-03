9a32f83f0b6de987adb7ff898c73b51b7920c168
lucasbrito0611 <lucasbrito2006@hotmail.com>
2025-05-10 18:24:37 -0300

    Organização dos arquivos e componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,58 +2,11 @@
 
 import type React from "react";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import dados, { TarefaInterface } from "@/data";
 import Cabecalho from "@/componentes/Cabecalho";
-
-interface TarefaProps {
-	titulo: string;
-	concluido?: boolean;
-}
-
-const Tarefa: React.FC<TarefaProps> = ({ titulo, concluido }) => {
-	const [estaConcluido, setEstaConcluido] = useState(concluido);
-
-	const classeCard = `p-3 mb-3 rounded-lg shadow-md hover:cursor-pointer hover:border ${
-		estaConcluido
-			? "bg-gray-800 hover:border-gray-800"
-			: "bg-gray-400 hover:border-gray-400"
-	}`;
-
-	const classeCorDoTexto = estaConcluido ? "text-amber-50" : "";
-
-	const escutarClique = () => {
-		console.log(`A tarefa '${titulo}' foi clicada!`);
-		setEstaConcluido(!estaConcluido);
-	};
-
-	return (
-		<div className={classeCard} onClick={() => escutarClique()}>
-			<h3 className={`text-xl font-bold ${classeCorDoTexto}`}>{titulo}</h3>
-			<p className={`text-sm ${classeCorDoTexto}`}>
-				{estaConcluido ? "Concluída" : "Pendente"}
-			</p>
-		</div>
-	);
-};
-
-interface TareafasProps {
-	dados: TarefaInterface[];
-}
-
-const Tarefas: React.FC<TareafasProps> = ({ dados }) => {
-	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-			{dados.map((tarefa) => (
-				<Tarefa
-					key={tarefa.id}
-					titulo={tarefa.title}
-					concluido={tarefa.completed}
-				/>
-			))}
-		</div>
-	);
-};
+import Tarefa from "@/componentes/Tarefa";
+import Tarefas from "@/componentes/Tarefas";
 
 const Home = () => {
 	const tarefas: TarefaInterface[] = dados;
